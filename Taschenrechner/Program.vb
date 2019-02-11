400bb4de7d241e07615b9216e1fe023e59c2478d
ArnoMatzpohl <Arno.Matzpohl@gmail.com>
2019-02-11 13:50:04 +0100

    Taschenrechner 1.1

--- Taschenrechner/Program.vb
+++ Taschenrechner/Program.vb
@@ -2,18 +2,75 @@
 
     Sub Main()
         Console.Title = "Taschenrechner"
-        Console.WriteLine("Hallo")
+        Console.WriteLine("Willkommen beim Taschenrechner.")
+        Console.WriteLine("Bestätige die Schritte mit Enter")
+
+
+        Console.WriteLine("")
+
+a:
 
         Dim Zahl1 As Integer
         Dim Zahl2 As Integer
+        Dim Rechenoperation As String
 
-        Console.WriteLine("Jetzt die Zahl eingeben!")
+        Console.WriteLine("")
+
+        Console.WriteLine("erste Zahl eingeben...")
+
         Zahl1 = Console.ReadLine
 
-        Console.WriteLine("Jetzt die zweite Zahl eingeben!")
+        Console.WriteLine("")
+
+        Console.WriteLine("zweite Zahl eingeben...")
         Zahl2 = Console.ReadLine
 
-        Console.WriteLine("Ergebnis:" & Zahl1 + Zahl2)
+        Console.WriteLine("")
+
+        Console.WriteLine("Addition=+")
+        Console.WriteLine("Subtraktion=-")
+        Console.WriteLine("Multiplikation=*")
+        Console.WriteLine("Division=/")
+
+        Console.WriteLine("")
+
+        Console.WriteLine("Rechenoperation eingeben:")
+
+        Console.WriteLine("")
+
+        Rechenoperation = Console.ReadLine
+
+        'If Rechenoperation = "+" Then
+        '    Console.WriteLine("Ergebnis:" & Zahl1 + Zahl2)
+
+        'ElseIf Rechenoperation = "-" Then
+        '    Console.WriteLine("Ergebnis:" & Zahl1 - Zahl2)
+
+        'ElseIf Rechenoperation = "*" Then
+        '    Console.WriteLine("Ergebnis:" & Zahl1 * Zahl2)
+
+        'ElseIf Rechenoperation = "/" Then
+        '    Console.WriteLine("Ergebnis:" & Zahl1 / Zahl2)
+        'End If
+
+        Select Case Rechenoperation
+            Case "+", "Plus", "plus", "PLUS"
+                Console.WriteLine("Ergebnis:" & Zahl1 + Zahl2)
+            Case "-", "Minus", "minus", "MINUS"
+                Console.WriteLine("Ergebnis:" & Zahl1 - Zahl2)
+            Case "*", "Multiplikation", "multiplikation", "MULTIPLIKATION"
+                Console.WriteLine("Ergebnis:" & Zahl1 * Zahl2)
+            Case "/", "Division", "division", "DIVISON"
+                Console.WriteLine("Ergebnis:" & Zahl1 / Zahl2)
+        End Select
+
+        Console.WriteLine("")
+        Console.WriteLine("")
+
+        Console.WriteLine("Weiter rechnen = Enter
+Beenden = rotes x oben rechts")
+
+        GoTo a
 
         Console.ReadLine()
     End Sub
