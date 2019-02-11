7dc35a3101b39eb619dd462e08954c0e49029a5d
ArnoMatzpohl <Arno.Matzpohl@gmail.com>
2019-02-11 12:35:47 +0100

    Taschenrechner 1.0

--- Taschenrechner/Program.vb
+++ Taschenrechner/Program.vb
@@ -1,7 +1,21 @@
 ﻿Module Program
 
     Sub Main()
+        Console.Title = "Taschenrechner"
+        Console.WriteLine("Hallo")
 
+        Dim Zahl1 As Integer
+        Dim Zahl2 As Integer
+
+        Console.WriteLine("Jetzt die Zahl eingeben!")
+        Zahl1 = Console.ReadLine
+
+        Console.WriteLine("Jetzt die zweite Zahl eingeben!")
+        Zahl2 = Console.ReadLine
+
+        Console.WriteLine("Ergebnis:" & Zahl1 + Zahl2)
+
+        Console.ReadLine()
     End Sub
 
 End Module
