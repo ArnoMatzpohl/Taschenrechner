7f2b16ece3e3a28eeb6eb9959a3aa728a93874b7
ArnoMatzpohl <Arno.Matzpohl@gmail.com>
2019-02-14 16:09:47 +0100

    Hangman "zu ende" gemacht

--- Hangman/Module1.vb
+++ Hangman/Module1.vb
@@ -1,6 +1,102 @@
 ﻿Module Module1
 
     Sub Main() ''Hauptprogram
+
+        Console.WriteLine("Gebe einen Text ein")
+        Dim EingegebenerText As String = Console.ReadLine
+
+        Dim WortlängeVonEingegebenerText As Integer
+        WortlängeVonEingegebenerText = EingegebenerText.Length
+        'Console.WriteLine(WortlängeVonEingegebenerText)
+
+        'For A As Integer = 0 To WortlängeVonEingegebenerText - 1
+        '    Console.WriteLine(EingegebenerText(A))
+        'Next
+
+        'For C As Integer = 2 To WortlängeVonEingegebenerText - 1
+        '    Console.WriteLine(EingegebenerText(C))
+        'Next
+
+        Dim B As String
+        B = EingegebenerText(0) ' B enthält das  erste Zeichen von EingegebenerText
+
+
+
+        'Console.WriteLine(WortEnthältBuchstabe("Welt", "F"))
+
+        Dim RichtigGeratendeBuchstaben As String
+
+        RichtigGeratendeBuchstaben = ""
+
+        Dim Fehlversuche As Integer
+
+        Dim RichtigGeratendeBuchstabenAnzahl As Integer
+
+
+
+
+        For T As Integer = 1 To 26
+            RichtigGeratendeBuchstabenAnzahl = 0
+            Console.Clear()
+            HangmanZeichnen(Fehlversuche)
+            Console.WriteLine("Gebe einen Buchstaben ein...")
+            Dim BuchstabeGeraten As String = Console.ReadLine
+            Console.WriteLine(WortEnthältBuchstabe(EingegebenerText, BuchstabeGeraten))
+
+            If WortEnthältBuchstabe(EingegebenerText, BuchstabeGeraten) Then 'Das Wort enthält den geratenden Buchstaben
+                RichtigGeratendeBuchstaben = RichtigGeratendeBuchstaben & BuchstabeGeraten
+                Console.WriteLine(RichtigGeratendeBuchstaben)
+                For R As Integer = 0 To EingegebenerText.Length - 1
+                    If WortEnthältBuchstabe(RichtigGeratendeBuchstaben, EingegebenerText(R)) Then
+                        RichtigGeratendeBuchstabenAnzahl += 1
+                    End If
+
+                Next
+
+                If RichtigGeratendeBuchstabenAnzahl = EingegebenerText.Length Then
+                    Console.WriteLine("Herzlichen Glückwunsch! Das Wort ist: " & EingegebenerText)
+                    Console.ReadLine()
+                End If
+            Else 'Das Wort enthält nicht den Buchstaben
+                Fehlversuche = Fehlversuche + 1
+
+            End If
+        Next
+
+
+
+
+        'Console.WriteLine(EingegebenerText("Welt", EingegebenerText))
+
+
+        Console.ReadLine()
+
+        'For Z As Integer = 2 To 20 Step 2 'Die SChleife aus Zahlen(zählen) hat den Startwert 2 und zählt bis 20 mit der Schrittweite 2
+        '    Console.WriteLine(Z)
+
+        'Next
+
+        'For I As Integer = 5 To 10
+
+        'Next
+
+        Dim Text As String
+        Text = "Hallo Welt"
+
+        Dim Wortlänge As Integer
+        Wortlänge = Text.Length
+
+        Console.WriteLine(Wortlänge)
+
+
+
+
+
+        Console.ReadLine()
+
+
+
+
         Dim currentDate = DateTime.Now
         Console.Title = "Hangman 1"
         Console.WriteLine("Datum/Uhrzeit: " & currentDate)
@@ -31,17 +127,7 @@
         Console.WriteLine("Wie ist dein Name?")
         Dim Name As String = Console.ReadLine
 
-        'Console.WriteLine(vbCrLf + "What is your name? ")
-        'Dim name = Console.ReadLine()
-        'Dim currentDate = DateTime.Now
-        'Console.WriteLine($"{vbCrLf}Hello, {Name}, on {currentDate:d} at {currentDate:t}")
-        'Console.Write(vbCrLf + "Press any key to exit... ")
-        'Console.ReadKey(True)
-
-        
         Console.Clear()
-
-
 
         Console.WriteLine("Datum/Uhrzeit: " & currentDate)
 
@@ -58,8 +144,6 @@
         Console.Clear()
 
         Dim EingegebeneBuchstaben As String = ""
-
-        Dim Fehlversuche As Integer = 0
 
 b:
         Console.Clear()
@@ -86,20 +170,20 @@ b:
 
 
 
-        Console.WriteLine(WortEnthältBuchstabe("Welt", "A")) 'Die Funktion mit Namen "WortEnthältBuchstabe" wird aufgerufen. Ihr wird der Wert "Welt", "A-G" als Parameter übergeben.
-        Console.WriteLine(WortEnthältBuchstabe("Welt", "B"))
-        Console.WriteLine(WortEnthältBuchstabe("Welt", "C"))
-        Console.WriteLine(WortEnthältBuchstabe("Welt", "D"))
-        Console.WriteLine(WortEnthältBuchstabe("Welt", "E"))
-        Console.WriteLine(WortEnthältBuchstabe("Welt", "F"))
-        Console.WriteLine(WortEnthältBuchstabe("Welt", "G"))
+        'Console.WriteLine(WortEnthältBuchstabe("Welt", "A")) 'Die Funktion mit Namen "WortEnthältBuchstabe" wird aufgerufen. Ihr wird der Wert "Welt", "A-G" als Parameter übergeben.
+        'Console.WriteLine(WortEnthältBuchstabe("Welt", "B"))
+        'Console.WriteLine(WortEnthältBuchstabe("Welt", "C"))
+        'Console.WriteLine(WortEnthältBuchstabe("Welt", "D"))
+        'Console.WriteLine(WortEnthältBuchstabe("Welt", "E"))
+        'Console.WriteLine(WortEnthältBuchstabe("Welt", "F"))
+        'Console.WriteLine(WortEnthältBuchstabe("Welt", "G"))
 
 
-        Dim Gurke As String
+        'Dim Gurke As String
 
-        Gurke = GeratendderBuchstabe
+        'Gurke = GeratendderBuchstabe
 
-        Console.WriteLine(WortEnthältBuchstabe("Welt", GeratendderBuchstabe))
+        'Console.WriteLine(WortEnthältBuchstabe("Welt", GeratendderBuchstabe))
 
         Console.ReadLine()
         GoTo b
@@ -116,17 +200,27 @@ a:
 
     End Sub
 
+
+    Function WortEnthältBuchstabe(EingegebenerText As String, Buchstabe As String) As Boolean
+
+        Return EingegebenerText.Contains(Buchstabe) 'es gibt den Wert zurück der dabei raus kommt wenn die Funktion Wort.CONTAINS mit dem Parameterwert von Buchstabe aufgerufen wird
+
+    End Function
+
+
+
+
+
+
+
+
     Function GeratenderBuchstabe() As String
         Console.WriteLine("Gebe einen buchstaben ein...")
         Dim EigegebenerBuchstabe As String = Console.ReadLine
         Return EigegebenerBuchstabe
     End Function
 
-    Function WortEnthältBuchstabe(Wort As String, Buchstabe As String) As Boolean
 
-        Return Wort.Contains(Buchstabe) 'es gibt den Wert zurück der dabei raus kommt wenn die Funktion Wort.CONTAINS mit dem Parameterwert von Buchstabe aufgerufen wird
-
-    End Function
 
     Sub HangmanZeichnen(Fehlversuche As Integer)
         Console.Clear()
