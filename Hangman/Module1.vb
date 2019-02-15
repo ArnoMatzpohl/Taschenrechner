725a1d49832142fa4246440da858b923531a2b41
ArnoMatzpohl <Arno.Matzpohl@gmail.com>
2019-02-15 12:32:48 +0100

    Neue

--- Hangman/Module1.vb
+++ Hangman/Module1.vb
@@ -2,101 +2,6 @@
 
     Sub Main() ''Hauptprogram
 
-        Console.WriteLine("Gebe einen Text ein")
-        Dim EingegebenerText As String = Console.ReadLine
-
-        Dim WortlängeVonEingegebenerText As Integer
-        WortlängeVonEingegebenerText = EingegebenerText.Length
-        'Console.WriteLine(WortlängeVonEingegebenerText)
-
-        'For A As Integer = 0 To WortlängeVonEingegebenerText - 1
-        '    Console.WriteLine(EingegebenerText(A))
-        'Next
-
-        'For C As Integer = 2 To WortlängeVonEingegebenerText - 1
-        '    Console.WriteLine(EingegebenerText(C))
-        'Next
-
-        Dim B As String
-        B = EingegebenerText(0) ' B enthält das  erste Zeichen von EingegebenerText
-
-
-
-        'Console.WriteLine(WortEnthältBuchstabe("Welt", "F"))
-
-        Dim RichtigGeratendeBuchstaben As String
-
-        RichtigGeratendeBuchstaben = ""
-
-        Dim Fehlversuche As Integer
-
-        Dim RichtigGeratendeBuchstabenAnzahl As Integer
-
-
-
-
-        For T As Integer = 1 To 26
-            RichtigGeratendeBuchstabenAnzahl = 0
-            Console.Clear()
-            HangmanZeichnen(Fehlversuche)
-            Console.WriteLine("Gebe einen Buchstaben ein...")
-            Dim BuchstabeGeraten As String = Console.ReadLine
-            Console.WriteLine(WortEnthältBuchstabe(EingegebenerText, BuchstabeGeraten))
-
-            If WortEnthältBuchstabe(EingegebenerText, BuchstabeGeraten) Then 'Das Wort enthält den geratenden Buchstaben
-                RichtigGeratendeBuchstaben = RichtigGeratendeBuchstaben & BuchstabeGeraten
-                Console.WriteLine(RichtigGeratendeBuchstaben)
-                For R As Integer = 0 To EingegebenerText.Length - 1
-                    If WortEnthältBuchstabe(RichtigGeratendeBuchstaben, EingegebenerText(R)) Then
-                        RichtigGeratendeBuchstabenAnzahl += 1
-                    End If
-
-                Next
-
-                If RichtigGeratendeBuchstabenAnzahl = EingegebenerText.Length Then
-                    Console.WriteLine("Herzlichen Glückwunsch! Das Wort ist: " & EingegebenerText)
-                    Console.ReadLine()
-                End If
-            Else 'Das Wort enthält nicht den Buchstaben
-                Fehlversuche = Fehlversuche + 1
-
-            End If
-        Next
-
-
-
-
-        'Console.WriteLine(EingegebenerText("Welt", EingegebenerText))
-
-
-        Console.ReadLine()
-
-        'For Z As Integer = 2 To 20 Step 2 'Die SChleife aus Zahlen(zählen) hat den Startwert 2 und zählt bis 20 mit der Schrittweite 2
-        '    Console.WriteLine(Z)
-
-        'Next
-
-        'For I As Integer = 5 To 10
-
-        'Next
-
-        Dim Text As String
-        Text = "Hallo Welt"
-
-        Dim Wortlänge As Integer
-        Wortlänge = Text.Length
-
-        Console.WriteLine(Wortlänge)
-
-
-
-
-
-        Console.ReadLine()
-
-
-
-
         Dim currentDate = DateTime.Now
         Console.Title = "Hangman 1"
         Console.WriteLine("Datum/Uhrzeit: " & currentDate)
@@ -138,29 +43,140 @@
         Console.WriteLine("")
         Console.WriteLine("-------------------")
         Console.WriteLine("")
-        Console.WriteLine("Gebe ein Wort ein, welches erraten werden soll...")
-        Dim Lösungswort = Console.ReadLine
+        'Console.WriteLine("Gebe ein Wort ein, welches erraten werden soll...")
+        'Dim Lösungswort = Console.ReadLine
 
-        Console.Clear()
+
+        Console.WriteLine("Gebe einen Text ein welcher erraten werden soll...") 'Es wird in die Konsole geschrieben "Gebe einen Text ein..."
+        Dim EingegebenerText As String = Console.ReadLine 'Die Variable "EingegebenerText" wird Definirt. Sie hat den Datentyp String. EingegebenerText übernimmt den Wert von Console.ReadLine
+        Dim WortlängeVonEingegebenerText As Integer ' Die Variable WortlängeVonEingegebenerText wird definirt und hat den Datentyp Integer
+        WortlängeVonEingegebenerText = EingegebenerText.Length 'Die länge von "EingegebenerText" wird WortlängeVonEingegebenerText übergeben
+        'Console.WriteLine(WortlängeVonEingegebenerText)
+
+        'For A As Integer = 0 To WortlängeVonEingegebenerText - 1
+        '    Console.WriteLine(EingegebenerText(A))
+        'Next
+
+        'For C As Integer = 2 To WortlängeVonEingegebenerText - 1
+        '    Console.WriteLine(EingegebenerText(C))
+        'Next
+
+        Dim B As String 'Dei Variable "B" wird definiert und hat den Dtatentyp String
+        B = EingegebenerText(0) ' B enthält das  erste Zeichen von EingegebenerText
+
+
+
+        'Console.WriteLine(WortEnthältBuchstabe("Welt", "F"))
+
+        Dim RichtigGeratendeBuchstaben As String 'Dei Variable "RichtigGeratendeBuchstaben" wird Definirt und hat den Datantyp String
+
+        RichtigGeratendeBuchstaben = "" '
+
+        Dim Fehlversuche As Integer 'Dei Variable "Fehlversuche" wird Definirt und hat den Datantyp String
+
+        Dim RichtigGeratendeBuchstabenAnzahl As Integer 'Dei Variable "RichtigGeratendeBuchstabenAnzahl" wird Definirt und hat den Datantyp String
+
+
+
+
+        For T As Integer = 1 To 26 ' Dei Schleife "T" Geht von 1 bis 26
+            RichtigGeratendeBuchstabenAnzahl = 0 'RichtigGeratendeBuchstabenAnzahl enthählt 0
+            Console.Clear() 'Die Konsole wird gelöscht
+            HangmanZeichnen(Fehlversuche) 'Es wird die Funktion Hangman Zeichnen aufgerufen und ihr wird Fehlversuche übergeben
+            Console.WriteLine("Gebe einen Buchstaben ein...") 'Es wird in die Konsole geschrieben: "Gebe einen Buchstaben ein..."
+            Dim BuchstabeGeraten As String = Console.ReadLine 'Die Variable BuchstabeGeraten wird Definiert, hat den Datentypen String und übernimmt den Wert von Console.Readline
+            Console.WriteLine(WortEnthältBuchstabe(EingegebenerText, BuchstabeGeraten)) 'Es wird überprüft ob der eingegebene Buchstabe in dem Eingegebenen test Enthalten ist.
+
+
+
+
+            If WortEnthältBuchstabe(EingegebenerText, BuchstabeGeraten) Then 'Das Wort enthält den geratenden Buchstaben dann:
+                RichtigGeratendeBuchstaben = RichtigGeratendeBuchstaben & BuchstabeGeraten 'Der Wert von RichtigGeratendeBuchstaben & BuchstabeGeraten Wird RichtigGeratendeBuchstaben zugewiesen
+                Console.WriteLine(RichtigGeratendeBuchstaben) 'In die Konsole wird das was In RichtigGeratendeBuchstaben steht geschrieben
+                For R As Integer = 0 To EingegebenerText.Length - 1 'Die schleife geht von 0 bis zu der Länge vom eingegebenen Text -1
+
+
+
+                    Dim EnthältBuchstabe As Boolean
+
+                    EnthältBuchstabe = WortEnthältBuchstabe(RichtigGeratendeBuchstaben, EingegebenerText(R))
+                    EnthältBuchstabe = RichtigGeratendeBuchstaben.Contains(EingegebenerText(R))
+
+                    If EnthältBuchstabe = True Then
+                        RichtigGeratendeBuchstabenAnzahl += 1 'zu RichtigGeratendeBuchstabenAnzahl wird einer dazu gezählt 
+                    End If
+
+                    'Das unter diesem satzt ist das gleiche wie über diesem Satz
+                    'If WortEnthältBuchstabe(RichtigGeratendeBuchstaben, EingegebenerText(R)) = True Then 'Es wird überprüft ob in RichtigGeratendeBuchstaben der  EingegebenerText(R)(R.ter Buchstabe von EingegebenerText) enthalten ist
+                    '    RichtigGeratendeBuchstabenAnzahl += 1 'zu RichtigGeratendeBuchstabenAnzahl wird einer dazu gezählt 
+                    'End If
+
+                Next
+
+                If RichtigGeratendeBuchstabenAnzahl = EingegebenerText.Length Then
+                    Console.WriteLine("Herzlichen Glückwunsch! Das Wort ist: " & EingegebenerText) 'In die Konsole wird geschrieben: "Herzlichen Glückwunsch! Das Wort ist: " & EingegebenerText
+                    Console.ReadLine()
+                End If
+            Else 'Das Wort enthält nicht den Buchstaben
+                Fehlversuche = Fehlversuche + 1 'Zu Fehversuchen wird 1 (Fehlversuch) hinzugefügt
+
+            End If
+        Next
+
+
+
+
+        'Console.WriteLine(EingegebenerText("Welt", EingegebenerText))
+
+
+        Console.ReadLine()
+
+        'For Z As Integer = 2 To 20 Step 2 'Die SChleife aus Zahlen(zählen) hat den Startwert 2 und zählt bis 20 mit der Schrittweite 2
+        '    Console.WriteLine(Z)
+
+        'Next
+
+        'For I As Integer = 5 To 10
+
+        'Next
+
+        Dim Text As String
+        Text = "Hallo Welt"
+
+        Dim Wortlänge As Integer
+        Wortlänge = Text.Length
+
+        Console.WriteLine(Wortlänge)
+
+
+
+
+
+        Console.ReadLine()
+
+
+
+
+
 
         Dim EingegebeneBuchstaben As String = ""
 
-b:
-        Console.Clear()
+        'b:
+        '        Console.Clear()
 
-        Dim currentDate4 = DateTime.Now
-        Console.WriteLine("Datum/Uhrzeit: " & currentDate4)
-        Console.WriteLine("")
+        '        Dim currentDate4 = DateTime.Now
+        '        Console.WriteLine("Datum/Uhrzeit: " & currentDate4)
+        '        Console.WriteLine("")
 
-        Console.WriteLine("Gebe einen Buchstaben ein...")
-        Dim GeratendderBuchstabe As String = Console.ReadLine
+        '        Console.WriteLine("Gebe einen Buchstaben ein...")
+        '        Dim GeratendderBuchstabe As String = Console.ReadLine
 
-        EingegebeneBuchstaben = EingegebeneBuchstaben & GeratendderBuchstabe
+        '        EingegebeneBuchstaben = EingegebeneBuchstaben & GeratendderBuchstabe
 
-        Select Case EingegebeneBuchstaben
-            Case Lösungswort
-                Console.WriteLine("Das Wort Wurde erraten. !!!Epischer Sieg!!!")
-        End Select
+        '        Select Case EingegebeneBuchstaben
+        '            Case Lösungswort
+        '                Console.WriteLine("Das Wort Wurde erraten. !!!Epischer Sieg!!!")
+        '        End Select
 
 
 
@@ -185,8 +201,8 @@ b:
 
         'Console.WriteLine(WortEnthältBuchstabe("Welt", GeratendderBuchstabe))
 
-        Console.ReadLine()
-        GoTo b
+        'Console.ReadLine()
+        'GoTo b
 
 
 a:
@@ -201,9 +217,9 @@ a:
     End Sub
 
 
-    Function WortEnthältBuchstabe(EingegebenerText As String, Buchstabe As String) As Boolean
+    Function WortEnthältBuchstabe(Wort As String, Buchstabe As String) As Boolean
 
-        Return EingegebenerText.Contains(Buchstabe) 'es gibt den Wert zurück der dabei raus kommt wenn die Funktion Wort.CONTAINS mit dem Parameterwert von Buchstabe aufgerufen wird
+        Return Wort.Contains(Buchstabe) 'es gibt den Wert zurück der dabei raus kommt wenn die Funktion Wort.CONTAINS mit dem Parameterwert von Buchstabe aufgerufen wird
 
     End Function
 
