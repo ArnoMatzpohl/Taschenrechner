a89a82dc6d47c0f292356a935bd966da9816bff9
ArnoMatzpohl <Arno.Matzpohl@gmail.com>
2019-02-20 13:03:30 +0100

    Passwortgenerator

--- Spielerein/Module1.vb
+++ Spielerein/Module1.vb
@@ -1,32 +1,54 @@
 ﻿Module Module1
 
-    Sub Main()
-        Dim Franz As String = Kleinbuchstaben1("BAUM")
-        Dim Heidi As String = Kleinbuchstaben2("au")
-        zeichneBisherGerateneBuchstaben(Franz, Heidi)
-    End Sub
 
-    Function Kleinbuchstaben1(BAUM As String) As String
-        Return BAUM.ToLower
-    End Function
-    Function Kleinbuchstaben2(au As String) As String
-        Return au.ToLower
-    End Function
 
-    Sub zeichneBisherGerateneBuchstaben(Wort As String, richtigeBuchstaben As String)
 
-        For Zähler As Integer = 0 To Wort.Length - 1
 
-            If richtigeBuchstaben.Contains(Wort(Zähler)) Then
-                Console.Write(Wort(Zähler) & " ")
 
-            Else
-                Console.Write("_ ")
-            End If
 
-        Next
-        Console.WriteLine()
-    End Sub
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    'Sub Main()
+    '    Dim Franz As String = Kleinbuchstaben1("BAUM")
+    '    Dim Heidi As String = Kleinbuchstaben2("au")
+    '    zeichneBisherGerateneBuchstaben(Franz, Heidi)
+    'End Sub
+
+    'Function Kleinbuchstaben1(BAUM As String) As String
+    '    Return BAUM.ToLower
+    'End Function
+    'Function Kleinbuchstaben2(au As String) As String
+    '    Return au.ToLower
+    'End Function
+
+    'Sub zeichneBisherGerateneBuchstaben(Wort As String, richtigeBuchstaben As String)
+
+    '    For Zähler As Integer = 0 To Wort.Length - 1
+
+    '        If richtigeBuchstaben.Contains(Wort(Zähler)) Then
+    '            Console.Write(Wort(Zähler) & " ")
+
+    '        Else
+    '            Console.Write("_ ")
+    '        End If
+
+    '    Next
+    '    Console.WriteLine()
+    'End Sub
 
     'schreibe eine funktion die nichts zurückgibt mit namen "zeichneBisherGerateneBuchstaben"
     'diese funktion enthält 2 Parameter
---
+++ test2/Form1.vb
@@ -0,0 +1,71 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim Haus As String
+        Dim Baum As String
+        Dim Auto As String
+
+
+        Haus = "Hallo"
+        Baum = "Welt"
+
+        Auto = Haus & "!" & Baum
+        Auto = Auto & "!"
+
+        Dim Schleife As Integer
+
+        Dim RandomZahl1 As New Random()
+
+        Randomize()
+        TextBox1.Clear()
+
+        For Hauptschleife As Integer = 1 To 30
+
+            Dim Zahl2 As Integer
+            Dim RandomZahl As Integer
+            Dim Text As String
+
+            'For Schleife2 As Long = 1 To Long.MaxValue
+            Dim Zahl1 As Integer = RandomZahl1.Next(65, 91)
+
+            Text = Chr(Zahl1)
+
+
+
+            RandomZahl = RandomZahl1.Next(1, 3)
+
+            If RandomZahl = 1 Then
+                Text = Text.ToLower
+            End If
+
+            'TextBox1.Text = TextBox1.Text & Text
+
+            'Next
+
+            ' For SchleifeZahlen As Integer = 0 To 15
+
+            Zahl2 = RandomZahl1.Next(0, 10)
+
+            'TextBox1.Text = TextBox1.Text & Zahl2
+
+            'Next
+
+            Dim RandomZahl2 As Integer = RandomZahl1.Next(1, 3)
+            'es soll entweder eine Zahl oder ein buchstabe ausgewählt werden und in die texbox eingefügt werden 
+            If RandomZahl2 = 1 Then
+                TextBox1.Text = TextBox1.Text & Zahl2
+            Else
+                TextBox1.Text = TextBox1.Text & Text
+            End If
+
+
+
+        Next
+
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
